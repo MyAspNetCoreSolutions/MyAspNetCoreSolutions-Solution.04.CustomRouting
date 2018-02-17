52fef87452bd6a63234ea612732d3fc1a09326bc
Lotfi <Lotfi@DESKTOP-8HS09L5>
2018-02-17 20:39:33 +0330

    Add project files.

---
+++ Session_04_CustomeRouting/MyCustomRouting.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session_04_CustomeRouting
+{
+    public class MyCustomRouting : IRouter
+    {
+        /// <summary>
+        /// Generate New Address
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get Urls
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Task RouteAsync(RouteContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
